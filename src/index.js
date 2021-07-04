40d885a3cf51d2e5e8b861def82fb0115b857b24
EnriqueTests <enriquenada@outlook.es>
2021-07-04 13:48:07 -0500

    Propiedad clave y separar propiedades

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import './index.css';
 
 const books = [
   {
+    id: 1,
     img: 'https://images-na.ssl-images-amazon.com/images/I/41gLTMZk62L._SX333_BO1,204,203,200_.jpg',
     title: 'Titulo del libro 1',
     author: 'Autor anonimo 1',
   },
   {
+    id: 2,
     img: 'https://images-na.ssl-images-amazon.com/images/I/41WOfByjSPL._SX331_BO1,204,203,200_.jpg',
     title: 'Titulo del libro 2',
     author: 'Autor anonimo 2',
   },
   {
+    id: 3,
     img: 'https://images-na.ssl-images-amazon.com/images/I/41NfAUwZbeL._SX308_BO1,204,203,200_.jpg',
     title: 'Titulo del libro 3',
     author: 'Autor anonimo 3',
@@ -23,19 +26,14 @@ const books = [
 function BookList() {
   return (
     <section className="booklist">
-      {books.map((book) => {
-        return (
-          <Book book={book}></Book>
-        );
+      {books.map((book, index) => {
+        return <Book key={book.id} {...book}></Book>
       })}
     </section>
   );
 }
 
-const Book = (props) => {
-  console.log(props);
-  const { img, title, author } = props.book;
-
+const Book = ({ img, title, author }) => {
   return (
     <article className='book'>
       <img src={img} alt="" />
